66b8866fa21cfd00981c0fd8dea2b95483ea9549
DiegoBarrosA <diegobarrosaraya@outlook.com>
2024-09-28 12:07:57 -0300

    Implemented unit test for Rooms

---
+++ src/test/java/com/aburaya/hotel/repository/StudentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.aburaya.hotel.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.aburaya.hotel.model.Room;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
+public class StudentRepositoryTest {
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Test
+    public void saveRoomTest() {
+        Room room = new Room();
+        room.setName("My room");
+        Room result = roomRepository.save(room);
+        assertNotNull(result.getId());
+        assertEquals("My room",result.getName());
+    }
+
+}
